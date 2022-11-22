5f4592fd4f10905dcc7a8a7164389ad05bbbfdd0
Prince Musonda <115486288+prince-musonda@users.noreply.github.com>
2022-11-22 12:24:06 +0200

    Add solution for tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  let conventedValue = (value - 32) * 5/9
+  roundedTemperatureValue = conventedValue.toFixed(1);
+  return Number(roundedTemperatureValue) 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  let conventedValue = value * 9/5 + 32
+  roundedTemperatureValue = conventedValue.toFixed(1);
+  return Number(roundedTemperatureValue)
 };
 
 // Do not edit below this line

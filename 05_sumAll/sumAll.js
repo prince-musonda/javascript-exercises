8bab1030f03a04d60c5af927c68a5babdf8c2d28
Prince Musonda <115486288+prince-musonda@users.noreply.github.com>
2022-11-22 11:54:10 +0200

    Add solution for sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+let counter = firstNum;
+let sum = 0;
 
+//ensuring that the input or argments are not negative numbers ...
+// ensuring that the input or argument is type of number before doing any calculation 
+isInputNumber = (typeof firstNum === 'number' ) && (typeof secondNum === 'number')
+isInputNegative = (firstNum < 0) || (secondNum < 0)
+if(isInputNumber === false || isInputNegative == true){
+    return 'ERROR'
+}
+
+//when the second argument is greater than the first
+if (firstNum < secondNum){
+    while(counter <= secondNum){
+        sum += counter;
+        counter++;
+    }
+
+}
+//when the first argument is greater than the second
+else if(firstNum > secondNum){
+    while(counter >= secondNum){
+        sum += counter;
+        counter--;
+    }
+}
+
+
+return sum
 };
 
 // Do not edit below this line

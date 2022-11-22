d144b179cfd6b5e31652222a6d733b4976e5e5f5
Prince Musonda <115486288+prince-musonda@users.noreply.github.com>
2022-11-22 11:07:03 +0200

    Add solution for removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
 const removeFromArray = function() {
-
+    
+    let numberOfArg =  arguments.length;
+    let targetArr = arguments[0]
+    for(let counter = 1;counter < numberOfArg; counter ++){
+     
+        let indexOfValueToRemove = targetArr.indexOf(arguments[counter]);
+        //ignore non present values in the array
+        if(indexOfValueToRemove !== -1){ 
+            targetArr.splice(indexOfValueToRemove,1);// remove item from array
+        }
+        
+       
+    }
+    
+    return targetArr;
 };
 
 // Do not edit below this line

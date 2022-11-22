01c65f0dae016a99dd12a73fd6ebd0a6aa680309
Prince Musonda <115486288+prince-musonda@users.noreply.github.com>
2022-11-22 12:08:36 +0200

    Add solution for leapYears excercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //a year is leap year if it's divisable by 4 and not 100
+    if(year % 4 === 0 && year % 100 !== 0){
+        return true
+    }
+    // if a year instead is divisable by 100, and also divisable by 400 then 
+    // it is a leap year also
+    else if(year % 100 === 0 && year % 400 === 0 ){
+        return true
+    }
+    //if a year is divisable by 100 but not by 400 then it is NOT a leap year
+    else{
+        return false
+    }
 };
 
 // Do not edit below this line
